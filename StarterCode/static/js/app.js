a57d8606f7fdabe919531fcb54fcb74aa9589ba6
nkeplinger <keplinger.nicolette@gmail.com>
2023-02-06 23:15:52 -0600

    final push

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,28 +1,27 @@
-
 const url= "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 // JSON DATA LOCATION "../samples.json"
 
 //STEP 1: READ IN DATA VIA D3
-d3.json('..data//samples.json').then(function(data) {
+d3.json(url).then(function(data) {
     console.log(data);
   });
 
 // CODE CAN MAKE GRAPHS, but cannot get to select the testSubject ID to modify/specify each one by drop down value. 
-//Needs to be some function for selecting whatever the drop down in for patint id and set to variable "testSubjectID" for plotting in functions below
+//Needs to be some function for selecting whatever the drop down in for patint id and set to variable "sampleId" for plotting in functions below
 
 
 // Create plots of data
-function makePlots(testSubjectID) {
-    data = d3.json('..data//samples.json').then(data => { 
+function makePlots(sampleId) {
+    let data = d3.json(url).then((data => { 
         // get all three data for flot from sameles key in json (sample_values(in desc order), otu_ids, and otu,ladels) 
         var samples = data.samples
         //filter sample by test subject
-        var SampleTestSubject = samples.filter(bacteriaInfo => bacteriaInfo.id == testSubjectID)[0]
+        var SampleTestSubject = samples.filter(Samples => Samples.id == sampleId)[0]
 
         //get metadata 
         var metadata = data.metadata
         //filter by selected test subject
-        var MetadataTestSubject = metadata.filter(bacteriaInfo => bacteriaInfo.id == testSubjectID)[0]
+        var MetadataTestSubject = metadata.filter(Samples => Samples.id == sampleId)[0]
 
         
         // Use sample_values as the x values for the bar chart.
@@ -36,9 +35,9 @@ function makePlots(testSubjectID) {
         //horizontal bar chart Data
         var barData = [{
             type: 'bar',
-            x: values.slice(0, 10),
-            y: labels.slice(0, 10),
-            text: hovertext.slice(0, 10),
+            x: sample_values.slice(0, 10),
+            y: otu_ids.slice(0, 10).map(otu_id => `OTU ${otu_id}`),
+            text: otu_labels.slice(0, 10),
             orientation: 'h'
         }];
         //horizontal bar chart layout
@@ -47,7 +46,7 @@ function makePlots(testSubjectID) {
         };
         //Plot horizontal bar chart 
           Plotly.newPlot("bar", barData, barLayout);
-        })
+        //)
 
         //STEP 3: BUBBLE CHART
         var bubbleData = [{
@@ -68,24 +67,52 @@ function makePlots(testSubjectID) {
         };
         // Display plot
         Plotly.newPlot('bubble', bubbleData, bubbleLayout)
-};
+    }))
+};   
+
 
 
 //Step 4&5 can define metadata, but cannot figure out how to properly display it
-function displayMetaData(testSubjectID){
-    var metadatabox = d3.select("#sample-metadata");
-    
-    data = d3.json('..data//samples.json').then(data => { 
-        //get metadata 
+
+function displayMetaData(sampleId) {
+
+    var demoInfo = d3.select('#sample-metadata')
+
+    d3.json(url).then(data => {
         var metadata = data.metadata
-        //filter by selected test subject
-        var MetadataTestSubject = metadata.filter(bacteriaInfo => bacteriaInfo.id == testSubjectID)[0]
+        var MetadataTestSubject = metadata.filter(Samples  => Samples.id == sampleId)[0]
 
-        
-
-
+        console.log(MetadataTestSubject)
+        Object.entries(MetadataTestSubject).forEach(([key, value]) => {
+            demoInfo.append("p").text(`${key}: ${value}`)
+        })
     })
+}
+
+function optionChanged(sampleId) {
+    console.log(sampleId);
+    makePlots(sampleId);
+    displayMetaData(sampleId);
+}
+
+function initDashboard() {
+    let dropdown = d3.select("#selDataset")
+    var data = d3.json(url).then(data =>{
+        var sampleId = data.names;
+        sampleId.forEach(sampleId => {
+            dropdown.append("option").text(sampleId).property("value", sampleId)
+        })
+
+        //make fluid selections
+        //makePlots(sampleId[0]);
+        //displayMetaData(sampleId[0]);
+
+        //make stagnant selections
+        makePlots('940');
+        displayMetaData('940');
+    });
 };
 
+initDashboard();
 
     
